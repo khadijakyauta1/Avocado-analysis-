1db10e854898995db7a15e714d0b9f32c7f02144
khadijakyauta1 <135613852+khadijakyauta1@users.noreply.github.com>
2023-07-03 14:58:45 +0100

    Add files via upload

---
+++ avocado analysis .sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM avocado 
+
+DESCRIBE avocado 
+
+SELECT *
+FROM avocado 
+WHERE type = 'conventional'
+ORDER BY `Total Bags` DESC 
+LIMIT 5
+
+SELECT *
+FROM avocado 
+WHERE type = 'organic'
+ORDER BY `Total Bags` DESC 
+LIMIT 5
+
+select * from
+
+
